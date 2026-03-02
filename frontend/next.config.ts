80f17bf41f5522fb161a0dfcb9762839b6b57c22
ANISH A S <7717823I106@kce.ac.in>
2026-03-02 17:26:00 +0530

    error fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: "/api/:path*",
+          destination: "http://127.0.0.1:5000/api/:path*",
+        },
+      ],
+    };
   },
 };
 
